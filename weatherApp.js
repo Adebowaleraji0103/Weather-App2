f1a06b7d4c95b479607b45684fe6fabd35fb2448
Mubarak <adebowaleraji0103@gmail.com>
2025-08-09 17:55:41 +0100

    d

--- weatherApp.js
+++ weatherApp.js
@@ -3,4 +3,7 @@ const city = "Lagos,NG";
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 fetch(apiUrl)
-.then(response => response.json)
+.then(response => response.json())
+.then(data =>{
+    console.log(data);
+})
