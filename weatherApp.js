d874e3093592aa33baf545c2ba68829e49e91790
Mubarak <adebowaleraji0103@gmail.com>
2025-08-12 18:21:35 +0100

    d

--- weatherApp.js
+++ weatherApp.js
@@ -2,6 +2,8 @@ const apiKey = "b331259393768ae562a5ec0b002648e4";
 const city = "Lagos,NG";
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+formElement = document.getElementById(form)
+
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
@@ -11,11 +13,11 @@ fetch(apiUrl)
     const weatherCondition = data.weather[0].description;
     const feelLikeTemperature = data.main.feels_like;
 
-    console.log(`city: ${cityName}`);
-    console.log(`temperature: ${temperature}`);
-    console.log(`humidity: ${humidity}`);
-    console.log(`weatherCondition: ${weatherCondition}`);
-     console.log(`feel-Like-Temperature ${feelLikeTemperature}`);
+    // console.log(`city: ${cityName}`);
+    // console.log(`temperature: ${temperature}`);
+    // console.log(`humidity: ${humidity}`);
+    // console.log(`weatherCondition: ${weatherCondition}`);
+    //  console.log(`feel-Like-Temperature ${feelLikeTemperature}`);
 
     // console.log(data)
   })
