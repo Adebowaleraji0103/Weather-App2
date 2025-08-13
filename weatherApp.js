7fba5a72e904e53402ef60203b411801dcff899e
Mubarak <adebowaleraji0103@gmail.com>
2025-08-13 12:17:37 +0100

    d

--- weatherApp.js
+++ weatherApp.js
@@ -3,6 +3,7 @@ const city = "Lagos,NG";
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 formElement.addEventListener("submit", (e) => {
+    e.preventDefault();    //to stop reloading
     const city = inputElement.value;
 })
 
